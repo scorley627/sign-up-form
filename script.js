4ad53fde850dadc021a0e08eb542023126c1ac90
Sean Corley <200403627+scorley627@users.noreply.github.com>
2025-05-07 15:03:58 -0600

    Add check to ensure passwords match

--- script.js
+++ script.js
@@ -1,19 +1,43 @@
 document.addEventListener("DOMContentLoaded", handleDOMContentLoaded);
 
 function handleDOMContentLoaded() {
-  const phoneField = document.querySelector("input[type='tel']");
-  phoneField.addEventListener("input", handlePhoneFieldInput);
+  const phoneField = document.getElementById("phone");
+  phoneField.addEventListener("input", handleInput);
+
+  const confirmPasswordField = document.getElementById("password_confirm");
+  confirmPasswordField.addEventListener("input", handleInput);
 }
 
-function handlePhoneFieldInput(event) {
+function handleInput(event) {
+  const field = event.target;
+  const isPhoneField = field.getAttribute("type") == "tel";
+  const isConfirmPasswordField = field.getAttribute("type") == "password";
+
+  if (isPhoneField) {
+    handlePhoneInput(field);
+  } else if (isConfirmPasswordField) {
+    handleConfirmPasswordInput(field);
+  }
+}
+
+function handlePhoneInput(phoneField) {
   const phoneChars = /^[0-9+\- ]*$/;
-  if (!phoneChars.test(event.target.value)) {
-    event.target.value = event.target.value.replace(/[^0-9+\- ]/g, "");
+  if (!phoneChars.test(phoneField.value)) {
+    phoneField.value = phoneField.value.replace(/[^0-9+\- ]/g, "");
   }
 
-  if (event.target.validity.patternMismatch) {
-    event.target.setCustomValidity("Please enter a valid phone number.");
+  if (phoneField.validity.patternMismatch) {
+    phoneField.setCustomValidity("Please enter a valid phone number.");
   } else {
-    event.target.setCustomValidity("");
+    phoneField.setCustomValidity("");
+  }
+}
+
+function handleConfirmPasswordInput(confirmPasswordField) {
+  const passwordField = document.getElementById("password");
+  if (passwordField.value != confirmPasswordField.value) {
+    confirmPasswordField.setCustomValidity("Password does not match.");
+  } else {
+    confirmPasswordField.setCustomValidity("");
   }
 }
